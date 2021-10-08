a74dee56e2736ddecfe0a3a59327b02c561a265b
丹文 柯 <108111114@mail.aeust.edu.tw>
2021-10-08 10:45:47 +0800

    HW2

---
+++ 110-1HW2/StuScore.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW2
+{
+    public partial class StuScore : System.Web.UI.Page
+    {
+        public class Scores
+        {
+            public string s_Id;
+            public float f_ChiS;
+            public float f_MathS;
+            public float f_EngS;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Scores o_Scores = new Scores();
+            o_Scores.s_Id = "A123456789";
+            o_Scores.f_ChiS = 62f;
+            o_Scores.f_MathS = 73f;
+            o_Scores.f_EngS = 84f;
+
+            Response.Write("s_Id=" + o_Scores.s_Id +
+                "," + "f_ChiS=" + o_Scores.f_ChiS.ToString() +
+                "," + "f_MathS=" + o_Scores.f_MathS.ToString() +
+                "," + "f_EngS=" + o_Scores.f_EngS.ToString());
+        
+        }
+    }
+}
